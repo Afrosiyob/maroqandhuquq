c12c01e396d21e4acb9e30cb13e70252462320b5
Afrosiyob <afrosiyob.a97@gmail.com>
2021-03-31 19:25:11 +0500

    add
    add

--- js/index.js
+++ js/index.js
@@ -1,18 +1,16 @@
-$(document).ready(function() {
+$( document ).ready( function () {
 
-    $('[data-toggle="tooltip"]').tooltip()
+    $( '[data-toggle="tooltip"]' ).tooltip()
 
-    $('.navbar-toggler').click(function(e) {
+    $( '.navbar-toggler' ).click( function ( e ) {
         e.preventDefault();
-        if ($(this).children('i').attr('class') == 'fas fa-bars') {
-            $(this).children('i').removeClass('fa-bars').addClass('fa-times');
+        if ( $( this ).children( 'i' ).attr( 'class' ) == 'fas fa-bars' ) {
+            $( this ).children( 'i' ).removeClass( 'fa-bars' ).addClass( 'fa-times' );
         } else {
-            $(this).children('i').removeClass('fa-times').addClass('fa-bars');
+            $( this ).children( 'i' ).removeClass( 'fa-times' ).addClass( 'fa-bars' );
         }
-    });
+    } );
 
 
 
-
-
-});
+} );
