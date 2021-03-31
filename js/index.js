5d83310e6cab39efa8623a7b40449b74ded2f29c
Afrosiyob <45900728+Afrosiyob@users.noreply.github.com>
2021-03-31 17:30:10 +0500

    add
    add

--- js/index.js
+++ js/index.js
@@ -1,21 +1,14 @@
 $(document).ready(function() {
 
+    $('[data-toggle="tooltip"]').tooltip()
+
     $('.navbar-toggler').click(function(e) {
         e.preventDefault();
-
-
-
-
         if ($(this).children('i').attr('class') == 'fas fa-bars') {
             $(this).children('i').removeClass('fa-bars').addClass('fa-times');
-
         } else {
             $(this).children('i').removeClass('fa-times').addClass('fa-bars');
         }
-
-
-
-
     });
 
 });
