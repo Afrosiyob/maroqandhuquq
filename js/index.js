af9af347aac5de1ae987f0d388d7f90074e8edee
Afrosiyob <45900728+Afrosiyob@users.noreply.github.com>
2021-04-01 12:54:21 +0500

    add
    add

--- js/index.js
+++ js/index.js
@@ -1,32 +1,29 @@
-$( document ).ready( function () {
+$(document).ready(function() {
 
-    $( '[data-toggle="tooltip"]' ).tooltip()
+    $('[data-toggle="tooltip"]').tooltip()
 
-    $( '.navbar-toggler' ).click( function ( e ) {
+    $('.navbar-toggler').click(function(e) {
         e.preventDefault();
-        if ( $( this ).children( 'i' ).attr( 'class' ) == 'fas fa-bars' ) {
-            $( this ).children( 'i' ).removeClass( 'fa-bars' ).addClass( 'fa-times' );
+        if ($(this).children('i').attr('class') == 'fas fa-bars') {
+            $(this).children('i').removeClass('fa-bars').addClass('fa-times');
         } else {
-            $( this ).children( 'i' ).removeClass( 'fa-times' ).addClass( 'fa-bars' );
+            $(this).children('i').removeClass('fa-times').addClass('fa-bars');
         }
-    } );
+    });
 
 
-    $( '.owl-carousel' ).owlCarousel( {
-        loop: true,
+    $('.owl-carousel').owlCarousel({
+        // loop: true,
         margin: 10,
         nav: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 3
-            },
-            1000: {
-                items: 5
-            }
-        }
-    } )
+        items: 1,
+        center: true,
+        stagePadding: 30,
+        autoHeight: true
 
-} );
+    });
+
+
+
+
+});
