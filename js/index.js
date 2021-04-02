e5200751bda1e7fb1a3f9d6a413721d874a42bdf
Afrosiyob <45900728+Afrosiyob@users.noreply.github.com>
2021-04-02 14:17:56 +0500

    add
    add

--- js/index.js
+++ js/index.js
@@ -1,5 +1,36 @@
 $(document).ready(function() {
+    $('.home-page-owl-carousel').owlCarousel({
 
+        margin: 10,
+        nav: true,
+        items: 1,
+        center: true,
+        stagePadding: 30,
+        autoHeight: true
+
+    });
+    $('.reagion-card-carousel').owlCarousel({
+
+        loop: false,
+        // center: true,
+        stagePadding: 30,
+        margin: 20,
+        nav: true,
+        autoHeight: true,
+        items: 5,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 5
+            }
+        }
+
+    });
     $('[data-toggle="tooltip"]').tooltip()
 
     $('.navbar-toggler').click(function(e) {
@@ -12,17 +43,66 @@ $(document).ready(function() {
     });
 
 
-    $('.owl-carousel').owlCarousel({
-        // loop: true,
-        margin: 10,
-        nav: true,
-        items: 1,
-        center: true,
-        stagePadding: 30,
-        autoHeight: true
+
+    const circleProgressBars = document.querySelectorAll('.progress-ring-circle');
+
+
+    circleProgressBars.forEach(circle => {
+
+        const radius = circle.r.baseVal.value;
+        const circumference = 2 * Math.PI * radius;
+        const getPercent = circle.getAttribute('percent');
+        var animationNumber = 0;
+
+
+        circle.style.strokeDasharray = `${circumference} ${circumference}`;
+        circle.style.strokeDashoffset = circumference;
+
+
+        function setProgress(percent) {
+            const offset = circumference - percent / 100 * circumference;
+            circle.style.strokeDashoffset = offset;
+
+            // $(circle).next('text').text(`${percent}%`);
+
+            var time = setInterval(() => {
+
+
+                $(circle).next('text').text(`${animationNumber}%`);
+
+                animationNumber += 1;
+
+                if (animationNumber > percent) {
+                    clearInterval(time);
+                    $(circle).next('text').text(`${percent}%`);
+                }
+
+            }, 100);
+
+
+
+
+        }
+
+        setProgress(getPercent);
 
     });
 
+
+    const lineProgressBars = document.querySelectorAll('.progress-line');
+
+
+    lineProgressBars.forEach(line => {
+
+        const getPercent = line.getAttribute('percent');
+
+        const childWidth = 100 - getPercent;
+
+        $(line).children('.progress-line-inner').css({
+            'width': `${childWidth}%`
+        });
+
+    });
 
 
 
