9fe431e6c164314549b7278089b452e2a41685f2
Afrosiyob <afrosiyob.a97@gmail.com>
2021-05-02 03:59:04 +0500

    add
    add

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
-   
+$( document ).ready( function () {
 
-    
-   
-    $('.home-page-owl-carousel').owlCarousel({
+
+
+
+    $( '.home-page-owl-carousel' ).owlCarousel( {
 
         margin: 10,
         nav: true,
@@ -12,8 +12,8 @@ $(document).ready(function() {
         stagePadding: 30,
         autoHeight: true
 
-    });
-    $('.reagion-card-carousel').owlCarousel({
+    } );
+    $( '.reagion-card-carousel' ).owlCarousel( {
 
         loop: false,
         // center: true,
@@ -34,86 +34,97 @@ $(document).ready(function() {
             }
         }
 
-    });
-    $('[data-toggle="tooltip"]').tooltip()
+    } );
+    $( '[data-toggle="tooltip"]' ).tooltip()
 
-    $('.navbar-toggler').click(function(e) {
+    $( '.navbar-toggler' ).click( function ( e ) {
         e.preventDefault();
-        if ($(this).children('i').attr('class') == 'fas fa-bars') {
-            $(this).children('i').removeClass('fa-bars').addClass('fa-times');
+        if ( $( this ).children( 'i' ).attr( 'class' ) == 'fas fa-bars' ) {
+            $( this ).children( 'i' ).removeClass( 'fa-bars' ).addClass( 'fa-times' );
         } else {
-            $(this).children('i').removeClass('fa-times').addClass('fa-bars');
+            $( this ).children( 'i' ).removeClass( 'fa-times' ).addClass( 'fa-bars' );
         }
-    });
+    } );
 
 
 
-    const circleProgressBars = document.querySelectorAll('.progress-ring-circle');
+    const circleProgressBars = document.querySelectorAll( '.progress-ring-circle' );
 
 
-    circleProgressBars.forEach(circle => {
+    circleProgressBars.forEach( circle => {
 
         const radius = circle.r.baseVal.value;
         const circumference = 2 * Math.PI * radius;
-        const getPercent = 100 - circle.getAttribute('percent');
+        const getPercent = 100 - circle.getAttribute( 'percent' );
         var animationNumber = 0;
 
 
-        circle.style.strokeDasharray = `${circumference} ${circumference}`;
+        circle.style.strokeDasharray = `${ circumference } ${ circumference }`;
         circle.style.strokeDashoffset = circumference;
 
 
-        function setProgress(percent) {
-            const offset = circumference - (percent) / 100 * circumference;
+        function setProgress ( percent ) {
+            const offset = circumference - ( percent ) / 100 * circumference;
             circle.style.strokeDashoffset = -offset;
 
             // $(circle).next('text').text(`${percent}%`);
 
-            var time = setInterval(() => {
+            var time = setInterval( () => {
 
 
-                $(circle).next('text').text(`${animationNumber}%`);
+                $( circle ).next( 'text' ).text( `${ animationNumber }%` );
 
                 animationNumber += 1;
 
-                if (animationNumber > (100 - percent)) {
-                    clearInterval(time);
-                    $(circle).next('text').text(`${(100 - percent)}%`);
+                if ( animationNumber > ( 100 - percent ) ) {
+                    clearInterval( time );
+                    $( circle ).next( 'text' ).text( `${ ( 100 - percent ) }%` );
                 }
 
-            }, 100);
+            }, 100 );
 
 
 
 
         }
 
-        setProgress(getPercent);
+        setProgress( getPercent );
 
-    });
-
-
-    const lineProgressBars = document.querySelectorAll('.progress-line');
+    } );
 
 
-    lineProgressBars.forEach(line => {
+    const lineProgressBars = document.querySelectorAll( '.progress-line' );
 
-        const getPercent = line.getAttribute('percent');
+
+    lineProgressBars.forEach( line => {
+
+        const getPercent = line.getAttribute( 'percent' );
 
         const childWidth = 100 - getPercent;
 
-        $(line).children('.progress-line-inner').css({
-            'width': `${childWidth}%`
-        });
+        $( line ).children( '.progress-line-inner' ).css( {
+            'width': `${ childWidth }%`
+        } );
 
-    });
+    } );
 
 
 
-    $('#buxoro').click(function(e) {
+    $( '#buxoro' ).click( function ( e ) {
         e.preventDefault();
         // alert("this is buhor");
-    });
+    } );
 
 
-});
+
+
+    const progressBox = $( '.progres-box' );
+
+    progressBox.click( function ( e ) {
+        e.preventDefault();
+        $( this ).next().slideToggle();
+        progressBox.not( this ).next().slideUp();
+    } );
+
+
+} );
